50ab9f674a4317a5661fb63b3db985777c6e79d2
deaklet <deaklet@hotmail.com>
2022-10-05 18:30:30 +1100

    jquery added

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){  
+    $(".home").hover(function(){  
+      $(this).attr('src','images/grey-home.png');  
+      }, function(){  
+      $(this).attr('src','images/tattoo.png');  
+    });  
+  });  
+
+$(document).ready(function(){  
+    $('.home').mousedown(function(){
+    $(this).attr('src', 'images/pink-home.png');
+});
+});  
+
+$(document).ready(function(){  
+    $('.home').mouseup(function(){
+    $(this).attr('src', 'images/grey-home.png');
+});
+});  
+
+
+  $(".aboutMe h2").click(function(){
+    $(".flexContainer").fadeToggle();
+  });
+  
+  $(".Projects h1").click(function(){
+    $(".workGrid").fadeToggle();
+  });
